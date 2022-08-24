992781c23772c4065a97e4b8ab86af3bbeb3f315
grom <takashi-san@yandex.ru>
2022-08-24 14:27:08 +0500

    lesson 6, lesson 12

---
+++ lesson-12-java-base/src/test/java/ru/otus/lesson_12/MainTest.java
@@ -0,0 +1,13 @@
+package ru.otus.lesson_12;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    void testMain() {
+        assertEquals(1, 2);
+    }
+}
