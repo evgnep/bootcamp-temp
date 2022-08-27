6505dd7c5cd96c8e34e139014bd213fa913c493e
Evgeny Nepomnyaschy <evgeny.nepomnyaschy@mvideo.ru>
2022-08-28 01:28:17 +0300

    Примеры программ для урока 9

---
+++ lesson-09-prog/manual/WeekdayExercise.java
@@ -0,0 +1,22 @@
+enum Weekday {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  // ...
+  // Объявляет enum Weekday {MONDAY … SUNDAY}
+}
+
+class WeekdayExercise {
+  public static void main(String[] args) {
+    // Берет первый параметр
+    // Преобразует его в число (подсказка: используйте метод Integer.parseInt(args[0]))
+    // var firstArg = ... ;
+
+    // Выводит название дня недели, соответствующего этому числу. Давайте считать, что понедельник имеет номер 1.
+    // (подсказка: используйте метод Weekday.values())
+    // System.out.println(...);
+  }
+}
---
+++ lesson-09-prog/manual/path.bat
@@ -0,0 +1,2 @@
+rem поставьте свой путь
+SET PATH=%PATH%;z:\java\jdk-17\bin
